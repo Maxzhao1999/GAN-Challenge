652d557fba0c5a5ddf6914a308afcc6f60a1fded
Max Zhao <maxzhao@hotmail.co.uk>
2020-09-02 18:59:03 +0800

    use old training loop

--- main.py
+++ main.py
@@ -11,19 +11,41 @@ import pstats
 import random
 # from pstats import SortKey
 import timeit
+import pathlib
 import re
 import io
-os.environ['KMP_DUPLICATE_LIB_OK']='True'
+import glob
+import PIL
+import PIL.Image
+os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
 # %%
-path = '/Users/Max/OneDrive - Imperial College London/4th yr project/GAN-Challenge/scenes'
 iters = int(sys.argv[1])
-
 print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
+data_dir = pathlib.Path('scenes/spirited_away/')
+images = list(data_dir.glob('*.jpeg'))
+
+batch_size = 32
+img_height = 32
+img_width = 32
+channels=3
+buffer_size=1024
+
+train_ds = tf.keras.preprocessing.image_dataset_from_directory(
+    'scenes', validation_split=0.2, subset="training", seed=123, image_size=(img_height, img_width),label_mode=None, batch_size=32)
+
+val_ds = tf.keras.preprocessing.image_dataset_from_directory(
+    'scenes', validation_split=0.2, subset="validation", seed=123, image_size=(img_height, img_width),  label_mode=None, batch_size=32)
+for element in train_ds.take(1).as_numpy_iterator():
+    image = element[0]
+
+# iters=10
+plt.imshow(image/max(np.concatenate(np.concatenate(image))))
+train_ds = train_ds.repeat(iters//train_ds.cardinality()+1).shuffle(buffer_size)
 
 # %%
 dropout = 0.4
 
-discriminator_input = keras.Input(shape=(28, 28, 1))
+discriminator_input = keras.Input(shape=(img_height, img_width, channels))
 discriminator_input.shape
 discriminator_input.dtype
 x = layers.Conv2D(16, 3, strides=2, activation='relu',
@@ -46,7 +68,7 @@ discriminator.compile(loss='binary_crossentropy',
 # %%
 dropout = 0.4
 depth = 64+64+64+64
-dim = 7
+dim = np.int(img_height/4)
 # In: 100
 # Out: dim x dim x depth
 generator_input = keras.Input(shape=(100,))
@@ -69,7 +91,7 @@ x = layers.Conv2DTranspose(int(depth/8), 5, padding='same')(x)
 x = layers.BatchNormalization(momentum=0.6)(x)
 x = layers.Activation('relu')(x)
 # Out: 28 x 28 x 1 grayscale image [0.0,1.0] per pix
-x = layers.Conv2DTranspose(1, 5, padding='same')(x)
+x = layers.Conv2DTranspose(channels, 5, padding='same')(x)
 generator_output = layers.Activation('sigmoid')(x)
 generator = keras.Model(generator_input, generator_output, name='generator')
 generator.summary()
@@ -86,7 +108,7 @@ y = np.append(np.ones(x_train.shape[0]),np.zeros(x_fake.shape[0]))
 x = np.append(x_train,x_fake,axis=0)
 #
 # discriminator.fit(x, y, epochs=1, batch_size=32)
-
+# train_ds.cardinality()
 # %%
 discriminator.trainable = False
 z = keras.Input(shape=(100,))
@@ -96,33 +118,47 @@ GAN = keras.Model(z,dis)
 GAN.compile(loss='binary_crossentropy', optimizer=optimizer,
 metrics=['accuracy'])
 GAN.summary()
-np.zeros((3,3))
 
 # %%
 # def train_on_n (batch_size=32):
     # generate images from noise
-batch_size = 32
-for i in range(iters):
+# batch_size = 32
+#
+# class GAN(keras.Model):
+#     def __init__(self, discriminator, generator):
+#         super(GAN, self).__init__()
+#         self.discriminator = discriminator
+#         self.generator = generator
+#
+#     def compile(self, d_optimizer, g_optimizer, loss_fn):
+#         super(GAN, self).compile()
+#         self.d_optimizer = d_optimizer
+#         self.g_optimizer = g_optimizer
+#         self.loss_fn = loss_fn
+#
+#     def train_step(self,real_images):
+iterator = iter(train_ds)
 
+for i in range(iters):
     noise_gen = np.random.rand(batch_size,100)
     generated_images = generator.predict(noise_gen)
 
     # load real images (for later use)
-    true_images = np.array(random.sample(list(x_train),batch_size))
-
+    true_images = iterator.get_next()
 
     # get discriminator prediction
     disc_out = np.random.rand(batch_size,1)*0.5+0.7
 
     # train discriminator (for later use)
-    x = np.append(true_images, generated_images,axis=0)
-    y = np.append(np.ones(true_images.shape[0]),np.zeros(generated_images.shape[0]))
+    # x = np.append(true_images, generated_images,axis=0)
+    # y = np.append(np.ones(true_images.shape[0]),np.zeros(generated_images.shape[0]))
 
     # train GAN
-    discriminator.train_on_batch(x,y)
+    discriminator.train_on_batch(x=true_images,y=np.ones(batch_size))
+    discriminator.train_on_batch(x=generated_images,y=np.zeros(generated_images.shape[0]))
+
     GAN.train_on_batch(noise_gen,disc_out)
     print("\r",i+1," out of ", iters, end="")
-
 
 
 # %%
@@ -144,15 +180,10 @@ ps.print_stats(10)
 rand = np.random.rand(1000,100)
 # GAN.train_on_batch(rand,[1])
 img = generator.predict(np.random.rand(1,100))
-img = img.reshape(28,28)
+img = img.reshape(img_height,img_width,channels)
 plt.imshow(img)
 
 plt.imsave("fig.png",img,dpi=300)
-
-eval = generator.predict(rand)
-eval = eval.reshape(1000,28,28,1)
-
-print("discriminator mean: ", np.mean(discriminator.predict(eval)))
 
 discriminator.save("discriminator_model.hdf5")
 generator.save("generator_model.hdf5")
