c7141589b0f88e2ff4e196263d30b19b443ccfae
Max Zhao <maxzhao@hotmail.co.uk>
2020-09-04 16:40:45 +0800

    increase max batch time to 3 hrs

--- job.sh
+++ job.sh
@@ -1,5 +1,5 @@
-#$-wd /vols/dune/zz5617/logs                                                                       
-#$-q gpu.q -l h_rt=1:0:0 
-#$-m ea -M zz5617@ic.ac.uk                                                                         
+#$-wd /vols/dune/zz5617/logs
+#$-q gpu.q -l h_rt=3:0:0 
+#$-m ea -M zz5617@ic.ac.uk
 cd /vols/dune/zz5617/GAN-Challenge
 python3 main.py ${SGE_TASK_ID}
